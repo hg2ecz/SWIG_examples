9050b90c69e074bd14a405c33dfb7abb3a055489
Krüpl Zsolt <hg2ecz@ham.hu>
2017-01-01 16:13:30 +0100

    +SWIG csharp

--- SWIG-C/example_main_as_class.cs
+++ SWIG-C/example_main_as_class.cs
@@ -22,7 +22,7 @@ namespace CExample
         public static void Main(string[] args)
         {
             Example a = new Example("Teszt-A");
-            Example b = new Example("Teszt-A");
+            Example b = new Example("Teszt-B");
 
             Console.WriteLine(a.Get());
             Console.WriteLine(b.Get());
