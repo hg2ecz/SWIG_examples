e07526bb99ca834a161880d29a0dbc71d7a755d5
Zsolt Krüpl <hg2ecz@ham.hu>
2018-04-05 22:06:22 +0200

    +speed

--- Rust-Python-func-bench/src/sourcesink4.rs
+++ Rust-Python-func-bench/src/sourcesink4.rs
@@ -7,36 +7,30 @@ pub fn source4(_py: Python, len: i32) -> PyResult<String> {
     let mut datastr = String::new();
 
     for i in 0..len {
-	let mut seven = [0u32; 5];
-	seven[0] = i as u32 >> (35-7);
-	seven[1] = (i as u32 >> (35-7-7)) & 0x7f;
-	seven[2] = (i as u32 >> (35-7-7-7)) & 0x7f;
-	seven[3] = (i as u32 >> (35-7-7-7-7)) & 0x7f;
-	seven[4] = i as u32 & 0x7f;
-
-	for j in 0..5 {
-	    let ch: char = unsafe { transmute::<u32, char>(seven[j as usize]) };
-	    datastr.push(ch);
+	unsafe {
+	    datastr.push(transmute::<u32, char>(i as u32 >> (35-7)));
+	    datastr.push(transmute::<u32, char>((i as u32 >> (35-7-7)) & 0x7f));
+	    datastr.push(transmute::<u32, char>((i as u32 >> (35-7-7-7)) & 0x7f));
+	    datastr.push(transmute::<u32, char>((i as u32 >> (35-7-7-7-7)) & 0x7f));
+	    datastr.push(transmute::<u32, char>(i as u32 & 0x7f));
 	}
     }
     Ok(datastr)
 }
 
 pub fn sink4(_py: Python, datastr: String) -> PyResult<i32>{
-    let mut i = 0u32;
-    let mut idx = 0usize;
-    let datavec: Vec<char> = datastr.chars().collect();                    // utf8 --> unicode32 ... slow
+    let datavec: Vec<char> = datastr.chars().collect();                // utf8 --> unicode32 ... slow
 
-    let mut seven = [0u32; 5];
-    for ch in datavec {
-	seven[idx as usize] = unsafe { transmute::<char, u32>(ch) };                // d: i32
-	if idx >= 4 {
-	    let d: u32 = (seven[0] << (35-7)) | (seven[1] << (35-7-7)) | (seven[2] << (35-7-7-7)) | (seven[3] << (35-7-7-7-7)) | seven[4];
-	    if d != i { println!("Error: compare failed ({} <> {})", i, d); }  // check
-	    idx=0; i+=1;
-	} else {
-	    idx+=1;
-	}
+    for i in 0..datastr.len()/5 {
+	let stridx = i*5;
+	let s0 = unsafe { transmute::<char, u32>(datavec[stridx+0]) }; // 4 bit
+	let s1 = unsafe { transmute::<char, u32>(datavec[stridx+1]) }; // 7 bit
+	let s2 = unsafe { transmute::<char, u32>(datavec[stridx+2]) }; // 7 bit
+	let s3 = unsafe { transmute::<char, u32>(datavec[stridx+3]) }; // 7 bit
+	let s4 = unsafe { transmute::<char, u32>(datavec[stridx+4]) }; // 7 bit
+	let d: u32 = (s0 << (35-7)) | (s1 << (35-7-7)) | (s2 << (35-7-7-7)) | (s3 << (35-7-7-7-7)) | s4;
+
+	if d != i as u32 { println!("Error: compare failed ({} <> {})", i, d); }  // check
     }
     Ok(0)
 }
