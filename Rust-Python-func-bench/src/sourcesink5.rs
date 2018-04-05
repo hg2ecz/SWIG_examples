bc9846810434a8991160782e4bdf31b450ba6274
Zsolt Krüpl <hg2ecz@ham.hu>
2018-04-05 21:46:44 +0200

    +speed

--- Rust-Python-func-bench/src/sourcesink5.rs
+++ Rust-Python-func-bench/src/sourcesink5.rs
@@ -22,21 +22,17 @@ pub fn source5(_py: Python, len: i32) -> PyResult<String> {
 }
 
 pub fn sink5(_py: Python, datastr: String) -> PyResult<i32>{
-    let mut i = 0u32;
-    let mut idx = 0usize;
     let ptr_u8 = datastr.as_ptr() as *const u8;
 
-    let mut seven = [0u32; 5];
-    for stridx in 0..datastr.len() {
-	seven[idx as usize] = unsafe { ptr::read( ptr_u8.offset(stridx as isize) ) } as u32;
-
-	if idx >= 4 {
-	    let d: u32 = (seven[0] << (35-7)) | (seven[1] << (35-7-7)) | (seven[2] << (35-7-7-7)) | (seven[3] << (35-7-7-7-7)) | seven[4];
-	    if d != i as u32 { println!("Error: compare failed ({} <> {})", i, d); }  // check
-	    idx=0; i+=1;
-	} else {
-	    idx+=1;
-	}
+    for i in 0..datastr.len()/5 {
+	let stridx: isize = i as isize * 5;
+	let s0 = unsafe { ptr::read( ptr_u8.offset(stridx+0) ) } as u32;
+	let s1 = unsafe { ptr::read( ptr_u8.offset(stridx+1) ) } as u32;
+	let s2 = unsafe { ptr::read( ptr_u8.offset(stridx+2) ) } as u32;
+	let s3 = unsafe { ptr::read( ptr_u8.offset(stridx+3) ) } as u32;
+	let s4 = unsafe { ptr::read( ptr_u8.offset(stridx+4) ) } as u32;
+	let d: u32 = (s0 << (35-7)) | (s1 << (35-7-7)) | (s2 << (35-7-7-7)) | (s3 << (35-7-7-7-7)) | s4;
+	if d != i as u32 { println!("Error: compare failed ({} <> {})", i, d); }  // check
     }
     Ok(0)
 }
